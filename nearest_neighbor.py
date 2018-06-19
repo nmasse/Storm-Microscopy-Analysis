dc530b64e0e6f5061d5f9ec474f8a4f01965d89d
nmasse <masse@uchicago.edu>
2018-06-19 00:10:15 -0500

    Change to interaction calculation

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -3,10 +3,11 @@ import csv
 import os
 import itertools
 import matplotlib
+import matplotlib.pyplot as plt
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 plt.rcParams["font.family"] = "Arial"
-import matplotlib.pyplot as plt
+
 from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import dbscan
 from itertools import product
@@ -18,6 +19,7 @@ class NN():
 
         self.data_dir = data_dir
 
+        """
         # dbscan parameters
         self.dbscan_max_dist = 50
         self.dbscan_min_samples = 3
@@ -27,9 +29,26 @@ class NN():
         self.dbscan_max_diameter = 1000
 
         # BIN1 interaction parameters
-        self.bin_interact_dist_threshold = 500
+        self.bin_interact_consideration_threshold = 500
         self.bin_interact_dist_threshold = 20
         self.bin_interact_contact_points = 10
+        """
+
+
+
+        # dbscan parameters
+        self.dbscan_max_dist = 50
+        self.dbscan_min_samples = 3
+        self.dbscan_min_samples_per_cluster = 25
+        self.dbscan_max_samples_per_cluster = 10000
+        self.dbscan_min_diameter = 25
+        self.dbscan_max_diameter = 1000
+
+        # BIN1 interaction parameters
+        self.bin_interact_consideration_threshold = 500
+        self.bin_interact_dist_threshold = 20
+        self.bin_interact_contact_points = 10
+
 
     def plot_supplemental_figure(self):
 
@@ -74,12 +93,18 @@ class NN():
 
 
 
-    def plot_main_results(self):
+    def plot_main_results(self, labels = ['GluA1','PSD95','CaMKII', 'Syn','Syp','Amph1','Bassoon']):
 
         NN_dist, subdirs, intearact_prob = self.search_dirs()
 
-        labels = ['GluA1','PSD95','CaMKII', 'Syn','Syp','Amph1','Bassoon']
-        order = [5,4,3,0,2,1,6]
+        order = []
+        for l in labels:
+            for i, sd in enumerate(subdirs):
+                if l in sd:
+                    order.append(i)
+
+        print(order)
+
         c = 0.1
 
         f = plt.figure(figsize=(8,4))
@@ -91,7 +116,7 @@ class NN():
         ax0 = plt.subplot(gs[0])
         for j, i in enumerate(order):
             u = np.linspace(j+1-c, j+1+c, len(intearact_prob[i]))
-            if i==3 or i==4 or i==5:
+            if 'PSD' in subdirs[i] or 'GluA1' in subdirs[i] or 'CaMKII' in subdirs[i]:
                 col = 'r'
                 post_scores.append(intearact_prob[i])
             else:
@@ -101,7 +126,7 @@ class NN():
             ax0.plot(u, intearact_prob[i], '.', color = col, markersize=8)
             ax0.plot([j+1-0.45, j+1+0.45],[np.median(intearact_prob[i]),np.median(intearact_prob[i])],'k')
 
-        ax0.set_xticks([1,2,3,4,5,6,7])
+        ax0.set_xticks([1,2,3,4,5,6])
         ax0.set_xticklabels(labels)
         ax0.set_ylabel('Proportion')
         ax0.set_ylim([0,0.07])
@@ -133,19 +158,18 @@ class NN():
 
         # We assume that all CSV files are contained in subdirectories
         dirs = [d for d in os.listdir(self.data_dir) if os.path.isdir(os.path.join(self.data_dir, d))]
-        print(dirs)
+        print('Working directories...', dirs)
 
         NN_dist = []
         subdirs = []
         interaction_prob = []
 
         for d in dirs:
+            #if 'MAP2' in d or (not 'Bassoon' in d and not 'SYP' in d and not 'Vgat' in d and not 'Syn' in d):
+            #if 'MAP2' in d or (not 'Bassoon_BPB' in d ):
             if 'MAP2' in d:
                 # don't know how to work with these files
                 continue
-
-            if not 'Bassoon' in d and not 'PSD' in d:
-                pass
 
             subdir = os.path.join(self.data_dir, d)
             files = [f for f in os.listdir(subdir) if os.path.isfile(os.path.join(subdir, f)) and 'csv' in f]
@@ -157,6 +181,7 @@ class NN():
             iteration = []
             cluster_center_mass = []
             cluster_labels = []
+            num_BIN_clusters = []
 
             # load and clean all the coordinates from all files within subdirector
             for f in files:
@@ -164,8 +189,10 @@ class NN():
                 k = f.find('.')
                 iteration.append(int(f[k-1]))
                 x = self.load_coords(os.path.join(subdir, f))
+                print('Current file ', f, ' iter ', iteration[-1], ' Bin ', BIN[-1])
                 lbs, cluster_cm, cluster_lbs = self.db_scan(x)
                 ind = np.where(lbs > -1)[0] # only use coords belonging to a cluster
+                print('Numer of pts, cluster pts, and pct ', len(lbs), len(ind), len(ind)/len(lbs))
                 coords.append(x[ind, :])
                 labels.append(lbs[ind])
                 cluster_center_mass.append(cluster_cm)
@@ -173,14 +200,14 @@ class NN():
                 #coords.append(x)
 
 
-            dist, mean_num_synaptic_coords, intearact_prob = self.calculate_BIN_interaction(coords, labels, \
+            dist, mean_num_synaptic_coords, intearact_prob , n_bin_clusters = self.calculate_BIN_interaction(coords, labels, \
                 BIN, iteration, cluster_center_mass, cluster_labels)
             #dist, mean_num_synaptic_coords = self.calculate_NN_distribution(coords, BIN, iteration)
             NN_dist.append(dist)
             subdirs.append(d)
             interaction_prob.append(intearact_prob)
             print(d, ' mean NN dist = ', np.mean(dist), ' num synaptic markers ', mean_num_synaptic_coords)
-            print( ' pct ', intearact_prob)
+            print( ' pct ', intearact_prob, 'num_BIN_clusters', n_bin_clusters)
 
         return NN_dist, subdirs, interaction_prob
 
@@ -256,7 +283,7 @@ class NN():
             if not len(ind)==2:
                 error('Issue ', ind)
 
-            if BIN[ind[0]]:
+            if BIN[ind[1]]:
                 bin_ind = ind[0]
                 syn_ind = ind[1]
             else:
@@ -275,25 +302,22 @@ class NN():
 
     def calculate_BIN_interaction(self, coords, labels, BIN, iteration, cluster_center_mass, cluster_labels):
 
-        self.bin_interact_consideration_threshold = 500
-        self.bin_interact_dist_threshold = 20
-        self.bin_interact_contact_points = 10
-
         dist = []
         num_synaptic_coords = []
         num_bin = []
         num_synaptic_marker = []
         interactions = []
         interactions_u = []
-
         interaction_prob = []
+        hits = []
+
+        print('len iteration', len(iteration))
 
         for i in np.unique(iteration):
             ind = np.where(iteration == i)[0]
             if not len(ind)==2:
                 error('Issue ', ind)
 
-            hits = 0
             total_possible = 0
             n_bin = 0
 
@@ -306,14 +330,18 @@ class NN():
             else:
                 bin_ind = ind[1]
                 syn_ind = ind[0]
+            print('number of bin clusters ', i, len(cluster_labels[bin_ind]))
 
 
             for j in range(len(cluster_labels[bin_ind])):
-
+                syn_marker_nearby = False
                 for k in range(len(cluster_labels[syn_ind])):
                     d = np.sqrt(np.sum((cluster_center_mass[bin_ind][j] - cluster_center_mass[syn_ind][k])**2))
                     if d < self.bin_interact_consideration_threshold:
-                        total_possible += 1
+                        if not syn_marker_nearby:
+                            hits.append(0)
+                            syn_marker_nearby = True
+
                         bin_cluster = np.where(labels[bin_ind] == cluster_labels[bin_ind][j])[0]
                         syn_cluster = np.where(labels[syn_ind] == cluster_labels[syn_ind][k])[0]
                         nbrs = NearestNeighbors(n_neighbors=1, algorithm='kd_tree').fit(coords[bin_ind][bin_cluster])
@@ -323,18 +351,19 @@ class NN():
                             nbrs = NearestNeighbors(n_neighbors=1, algorithm='kd_tree').fit(coords[syn_ind][syn_cluster])
                             current_dist, _ = nbrs.kneighbors(coords[bin_ind][bin_cluster])
                             if np.sum(current_dist < self.bin_interact_dist_threshold) >= self.bin_interact_contact_points:
-                                hits+=1
-                                num_bin.append(len(bin_cluster))
-                                num_synaptic_marker.append(len(syn_cluster))
-                                interactions.append(np.sum(current_dist < self.bin_interact_dist_threshold))
-                                interactions_u.append(np.mean(current_dist < self.bin_interact_dist_threshold))
+                                hits[-1] = 1
+                                break
+                                #num_bin.append(len(bin_cluster))
+                                #num_synaptic_marker.append(len(syn_cluster))
+                                #interactions.append(np.sum(current_dist < self.bin_interact_dist_threshold))
+                                #interactions_u.append(np.mean(current_dist < self.bin_interact_dist_threshold))
 
 
-            interaction_prob.append(hits/total_possible)
+            interaction_prob.append(np.mean(hits))
             dist.append(current_dist)
             #print('hits ', hits, 'misses ', misses, 'number BIN', n_bin, 'Hit cluster size ', np.mean(num_bin), np.mean(num_synaptic_marker))
             #print('sum and mean interactions per hit', np.mean(interactions), np.mean(interactions_u))
-        return list(itertools.chain(*dist)), np.mean(num_synaptic_coords), interaction_prob
+        return list(itertools.chain(*dist)), np.mean(num_synaptic_coords), interaction_prob, num_bin
 
     def load_coords(self, filename):
 
@@ -347,6 +376,7 @@ class NN():
         k = int(np.where(s1[0,:] == 'x [nm]')[0])
         coords = np.float32(s1[1:,k:k+3])
         #coords = self.eliminate_isolated_coords(coords)
+        #coords[:,-1] *= 0
         return coords
 
     def eliminate_isolated_coords(self, coords):
